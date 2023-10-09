46a9f53121818096df509a84ba447ca892347134
Ching Chhay <chingchhay.sam@wingmoney.com>
2023-10-09 17:44:21 +0700

    fixed: resolve duplicate key in TagList

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -6,8 +6,11 @@ export default function TagList({ items }: TTagList) {
   return (
     <ul className='flex flex-row'>
       {items.length > 0 &&
-        items.map((item) => (
-          <li className='mr-1 rounded-sm bg-blue-100 px-2 text-xs text-blue-600'>
+        items.map((item, idx) => (
+          <li
+            key={`${idx}_${item}`}
+            className='mr-1 rounded-sm bg-blue-100 px-2 text-xs text-blue-600'
+          >
             {item}
           </li>
         ))}
