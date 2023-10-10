977ccac9654d6cc1d87950b0ccedf44809d4641a
liqwik <liqwik.wang@gmail.com>
2023-10-10 19:59:12 +0700

    fixed (backend): set bot welcome message in env file

--- backend/main.js
+++ backend/main.js
@@ -2,23 +2,9 @@ import 'dotenv/config'
 import { Input, Markup, Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import { link } from "telegraf/format";
-import { BOT_TOKEN, WEB_APP_URL } from './config.js';
+import { BOT_TOKEN, BOT_WELCOME_MESSAGE, WEB_APP_URL } from './config.js';
 
 const bot = new Telegraf(BOT_TOKEN);
-
-const welcomeMessage = `🌟 Welcome to the Mini Linked! 🌟
-
-Are you looking to take the next step in your career? You're in the right place! Our powerful job search platform is designed to connect talented individuals like you with exciting career opportunities.
-
-With our user-friendly interface and comprehensive database of job listings, you can easily browse through a wide range of industries, professions, and locations. Whether you're a seasoned professional or just starting your career journey, we've got you covered.
-
-Discover job openings from top companies and organizations, explore detailed job descriptions, and apply directly through our app. We'll help you streamline your job search and make the process as smooth as possible.
-
-Stay up-to-date with the latest job market trends, industry insights, and expert career advice through our curated resources. We're here to empower you with the knowledge and tools you need to succeed in your job search.
-
-Remember, your dream job is just a few clicks away. Let's embark on this journey together and unlock endless career possibilities. Good luck!
-
-If you have any questions or need assistance, feel free to reach out to our support team. Happy job hunting! 🎉`;
 
 bot.start(async (ctx) => {
   await ctx.setChatMenuButton({
@@ -27,7 +13,7 @@ bot.start(async (ctx) => {
     web_app: { url: WEB_APP_URL },
   });
 
-  await ctx.reply(welcomeMessage,
+  await ctx.reply(BOT_WELCOME_MESSAGE,
     Markup.inlineKeyboard([Markup.button.webApp("View Jobs", WEB_APP_URL)])
   );
 });
